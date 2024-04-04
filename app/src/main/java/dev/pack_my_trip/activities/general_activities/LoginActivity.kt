e448925078a32416672e9ce481c6e5dad76e2869
juanfra312003 <juanframireze@javeriana.edu.co>
2024-04-04 17:00:32 -0500

    Fix: Binding and xml issue with login activity

--- app/src/main/java/dev/pack_my_trip/activities/general_activities/LoginActivity.kt
+++ app/src/main/java/dev/pack_my_trip/activities/general_activities/LoginActivity.kt
@@ -3,10 +3,14 @@ package dev.pack_my_trip.activities.general_activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import dev.pack_my_trip.R
+import dev.pack_my_trip.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }
