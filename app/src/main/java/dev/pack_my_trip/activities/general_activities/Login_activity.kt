ff61c69d08fac2a227fc5ec629ed9598a163154b
juanfra312003 <juanframireze@javeriana.edu.co>
2024-04-04 16:33:57 -0500

    Feature: Organizacion actividades (Pantallas)

---
+++ app/src/main/java/dev/pack_my_trip/activities/general_activities/Login_activity.kt
@@ -0,0 +1,12 @@
+package dev.pack_my_trip.activities.general_activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dev.pack_my_trip.R
+
+class login_activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
