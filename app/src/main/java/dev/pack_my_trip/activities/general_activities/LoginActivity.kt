2ebaaddd516a8ab0373f15d05da5a97ad261bfcf
juanfra312003 <juanframireze@javeriana.edu.co>
2024-04-09 16:33:34 -0500

    Feature: XML View map

--- app/src/main/java/dev/pack_my_trip/activities/general_activities/LoginActivity.kt
+++ app/src/main/java/dev/pack_my_trip/activities/general_activities/LoginActivity.kt
@@ -14,6 +14,6 @@ class LoginActivity : AppCompatActivity() {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        //startActivity(Intent(this, DashboardTouristActivity::class.java))
+       startActivity(Intent(this, DashboardTouristActivity::class.java))
     }
 }
