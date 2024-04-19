a9602fb81a69c1a0910fba50615522f2967e4c5a
Valentina Garcia <valentinagarciaalfonso@gmail.com>
2024-04-18 22:06:24 -0500

    Cambios Valentina

---
+++ app/src/main/java/dev/pack_my_trip/activities/operator_activities/MetricasActivity.kt
@@ -0,0 +1,36 @@
+package dev.pack_my_trip.activities.tourist_activities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ListView
+import dev.pack_my_trip.R
+import dev.pack_my_trip.activities.operator_activities.MisServiciosAdapter
+import dev.pack_my_trip.activities.operator_activities.ServiciosMetricasAdapter
+import dev.pack_my_trip.databinding.ActivityMetricasBinding
+
+class MetricasActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMetricasBinding
+    private lateinit var misServiciosListView: ListView
+    private lateinit var misServicios2ListView: ListView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMetricasBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        inicializarVariables()
+    }
+
+    fun inicializarVariables(){
+        misServiciosListView = findViewById(R.id.MisServiciosListView)
+        val data = mapOf("a" to 1, "b" to 2)
+        val adapter = ServiciosMetricasAdapter(this, data)
+        misServiciosListView.adapter = adapter
+
+        misServicios2ListView = findViewById(R.id.MisServiciosListView)
+        val data2 = mapOf("a" to 1, "b" to 2)
+        val adapter2 = ServiciosMetricasAdapter(this, data2)
+        misServiciosListView.adapter = adapter2
+    }
+}
