c77288fab31bd7b80b22acf62907822c9e40c4c8
Édgar Julián González Sierra <99839239+Juligo17@users.noreply.github.com>
2024-04-24 20:08:00 -0500

    Delete app/src/main/java/dev/pack_my_trip/ConectionBack/OnGetServiciosPresenter.kt

--- app/src/main/java/dev/pack_my_trip/ConectionBack/OnGetServiciosPresenter.kt
+++
@@ -1,7 +0,0 @@
-package dev.pack_my_trip.ConectionBack
-
-import dev.pack_my_trip.models.data_model.Servicio
-
-fun interface OnGetServiciosPresenter {
-    fun onGetServiciosPresenter(servicios: List<Servicio>)
-}
